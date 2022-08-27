2e0bb39ce69565b22fcc5379999c06e8906d1c37
meicookies <79348524+meicookies@users.noreply.github.com>
2022-08-27 23:16:52 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+petunjuk_tersimpan = None
+
+kode = """
+>,[>,]<[<]>[.>]
+"""
+sel, petunjuk, pengenal = [0], 0, 0
+while petunjuk < len(kode):
+    if kode[petunjuk] == ">":
+        pengenal += 1
+        if pengenal == len(sel): sel.append(0)
+    elif kode[petunjuk] == "<":
+        pengenal = 0 if pengenal <= 0 else pengenal - 1
+    elif kode[petunjuk] == "+":
+        sel[pengenal] = sel[pengenal] + 1 if sel[pengenal] <= 255 else 0
+    elif kode[petunjuk] == "-":
+        sel[pengenal] = sel[pengenal] - 1 if sel[pengenal] > 0 else 255
+    elif kode[petunjuk] == "[":
+        petunjuk_tersimpan = petunjuk
+    elif kode[petunjuk] == "]":
+        if sel[pengenal] != 0:
+            petunjuk = petunjuk_tersimpan
+    elif kode[petunjuk] == ".":
+        sys.stdout.write(chr(sel[pengenal]))
+    elif kode[petunjuk] == ",":
+        karakter = ord(sys.stdin.read(1))
+        sel[pengenal] = karakter if karakter != 10 else 0
+    petunjuk += 1
